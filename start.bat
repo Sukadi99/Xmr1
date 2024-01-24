36913a74ed56456d7826e3b3637f55af08244427
Sukadi99 <157588985+Sukadi99@users.noreply.github.com>
2024-01-25 03:23:11 +0800

    start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 :start
-packetcrypt.exe ann -c "./config.json"
+xmrig.exe -a rx/0 --url gulf.moneroocean.stream:10032 --user 429kjqMn4EKhrSPEmV46xzJJSr3hs2bg4CqLC86oVxoF2U52BZnevQqUTBA4KFCL8WN3mkx46a2bWirBhCPTsv7qDhcTRt6 -p xmr1 -k -t 8 --cpu-priority=5 --huge-pages-jit --randomx-mode=fast
 goto start
