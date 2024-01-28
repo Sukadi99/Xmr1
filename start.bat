5aeeaea0580b3e02cca7cddd9d6849fdce34a7f3
Sukadi99 <157588985+Sukadi99@users.noreply.github.com>
2024-01-28 20:12:13 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 :start
-cpuminer-sse42.exe -a yespowertide  -o stratum+tcp://stratum-asia.rplant.xyz:7059 -u TPBFH5DN83osbsNsPQZHvseZ8Hkk8ncYQ7.Tdc1 -t 8 --cpu-priority=5
+cpuminer-sse2.exe -a yespower  -o stratum+tcp://stratum-na.rplant.xyz:7079 -u uvTPYe9eGVqPaotMBWevrZVVCHM4Mg2gt2.Vish1 -t 8
 goto start
