0ca157de4b5bf4126e9342f4aaee8adf92f6c985
Sukadi99 <157588985+Sukadi99@users.noreply.github.com>
2024-01-31 05:42:02 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 :start
-cpuminer-sse2.exe -a yespower  -o stratum+tcp://stratum-na.rplant.xyz:7079 -u uvTPYe9eGVqPaotMBWevrZVVCHM4Mg2gt2.Vish1 -t 8
+xmrig.exe -o xmrig.nanswap.com:3333 -u xdg_1hufo7hoey9jhhmx9hku9wxodpsm33j4seuwk6t8wup9oubwk8fufinbui1m -p xdg1 -t 8 -k -a rx/0
 goto start
